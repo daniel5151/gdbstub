a0abbefc152b3894f9ce010404d6ee2467980cd0
sapir <yasapir@gmail.com>
2020-03-29 20:38:33 +0300

    Allow target to trigger breakpoints itself (#6)
    * Allow target to trigger breakpoints itself
    
    * grammar nit
    
    * Rename TargetState::Breakpoint to SoftwareBreakpoint
    
    Co-authored-by: Daniel Prilik <danielprilik@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -376,6 +376,9 @@ pub enum TargetState {
     Running,
     /// Halted
     Halted,
+    /// Running, but system triggered a software breakpoint, e.g. due to a trap instruction.
+    /// This is not necessarily a breakpoint configured by the client/user.
+    SoftwareBreakpoint,
 }
 
 /// A trait for reading / writing bytes across some transport layer.
