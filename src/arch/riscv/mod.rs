5920a3cd35fc82fdca488285e9c489f909258a4f
Thomas Hiscock <thomashk0@users.noreply.github.com>
2020-08-31 15:33:15 +0200

    Add (partial) support for RISC-V (#21)
    * implement Arch for RISC-V 32/64
    
    Introduces support for **integer** RISC-V ISA.
    
    * fix minor style issues in the riscv module

---
+++ src/arch/riscv/mod.rs
@@ -0,0 +1,33 @@
+//! Support for the [RISC-V](https://riscv.org/) architecture.
+//!
+//! *Note*: currently only supports integer versions of the ISA.
+
+use crate::arch::Arch;
+
+pub mod reg;
+
+/// Implements `Arch` for 32-bit RISC-V.
+#[derive(Eq, PartialEq)]
+pub struct Riscv32;
+
+/// Implements `Arch` for 64-bit RISC-V.
+#[derive(Eq, PartialEq)]
+pub struct Riscv64;
+
+impl Arch for Riscv32 {
+    type Usize = u32;
+    type Registers = reg::RiscvCoreRegs<u32>;
+
+    fn target_description_xml() -> Option<&'static str> {
+        Some(r#"<target version="1.0"><architecture>riscv</architecture></target>"#)
+    }
+}
+
+impl Arch for Riscv64 {
+    type Usize = u64;
+    type Registers = reg::RiscvCoreRegs<u64>;
+
+    fn target_description_xml() -> Option<&'static str> {
+        Some(r#"<target version="1.0"><architecture>riscv64</architecture></target>"#)
+    }
+}
---
+++ src/arch/riscv/reg/mod.rs
@@ -0,0 +1,5 @@
+//! `GdbRegister` structs for RISC-V architectures.
+
+mod riscv;
+
+pub use riscv::RiscvCoreRegs;
