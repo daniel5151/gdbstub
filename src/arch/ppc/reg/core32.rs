670e22ff52a10d81b722bd7cb81fa8ffadded2ae
jamcleod <Jordan.McLeod@ll.mit.edu>
2020-09-03 15:10:51 -0400

    Add common 32-bit PowerPC support

--- src/arch/ppc/reg/core32.rs
+++ src/arch/ppc/reg/core32.rs
@@ -1,9 +1,40 @@
+use crate::arch::ppc::reg::PpcVector;
 use crate::arch::Registers;
 
-///
-#[derive(Default)]
-pub struct PowerPcCoreRegs {
+use core::convert::TryInto;
 
+/// 32-bit PowerPC core registers.
+///
+/// Sources:
+/// * https://github.com/bminor/binutils-gdb/blob/master/gdb/features/rs6000/powerpc-32.xml
+/// * https://github.com/bminor/binutils-gdb/blob/master/gdb/features/rs6000/power-core.xml
+/// * https://github.com/bminor/binutils-gdb/blob/master/gdb/features/rs6000/power-fpu.xml
+#[derive(Default, Debug, PartialEq)]
+pub struct PowerPcCoreRegs {
+    /// General purpose registers
+    pub r: [u32; 32],
+    /// Float registers
+    pub f: [f64; 32],
+    /// Program counter
+    pub pc: u32,
+    /// Machine state
+    pub msr: u32,
+    /// Condition register
+    pub cr: u32,
+    /// Link register
+    pub lr: u32,
+    /// Count register
+    pub ctr: u32,
+    /// Integer exception register
+    pub xer: u32,
+    /// Floating-point status and control register
+    pub fpscr: u32,
+    /// Vector registers
+    pub vr: [PpcVector; 32],
+    /// Vector status and control register
+    pub vscr: u32,
+    /// Vector context save register
+    pub vrsave: u32,
 }
 
 impl Registers for PowerPcCoreRegs {
@@ -16,129 +47,112 @@ impl Registers for PowerPcCoreRegs {
             };
         }
 
-        (0..932).for_each(|x| write_byte(Some(x as u8)))
-    }
-
-    fn gdb_deserialize(&mut self, bytes: &[u8]) -> Result<(), ()> {
-        Ok(())
-    }
-}
-
-/*
-/// 64-bit x86 core registers (+ SSE extensions).
-///
-/// Source: https://github.com/bminor/binutils-gdb/blob/master/gdb/features/i386/64bit-core.xml
-/// Additionally: https://github.com/bminor/binutils-gdb/blob/master/gdb/features/i386/64bit-sse.xml
-#[derive(Default)]
-pub struct X86_64CoreRegs {
-    /// RAX, RBX, RCX, RDX, RSI, RDI, RBP, RSP, r8-r15
-    pub regs: [u64; 16],
-    /// Status register
-    pub eflags: u32,
-    /// Instruction pointer
-    pub rip: u64,
-    /// Segment registers: CS, SS, DS, ES, FS, GS
-    pub segments: [u32; 6],
-    /// FPU registers: ST0 through ST7
-    pub st: [F80; 8],
-    /// FPU internal registers
-    pub fpu: X87FpuInternalRegs,
-    /// SIMD Registers: XMM0 through XMM15
-    pub xmm: [u128; 0x10],
-    /// SSE Status/Control Register
-    pub mxcsr: u32,
-}
-
-impl Registers for X86_64CoreRegs {
-    fn gdb_serialize(&self, mut write_byte: impl FnMut(Option<u8>)) {
-        macro_rules! write_bytes {
-            ($bytes:expr) => {
-                for b in $bytes {
-                    write_byte(Some(*b))
-                }
-            };
+        macro_rules! write_regs {
+            ($($reg:ident),*) => {
+                $(
+                    write_bytes!(&self.$reg.to_le_bytes());
+                )*
+            }
         }
 
-        // rax, rbx, rcx, rdx, rsi, rdi, rbp, rsp, r8-r15
-        for reg in &self.regs {
+        for reg in &self.r {
             write_bytes!(&reg.to_le_bytes());
         }
 
-        // rip
-        write_bytes!(&self.rip.to_le_bytes());
-
-        // eflags
-        write_bytes!(&self.eflags.to_le_bytes());
-
-        // cs, ss, ds, es, fs, gs
-        for seg in &self.segments {
-            write_bytes!(&seg.to_le_bytes());
+        for reg in &self.f {
+            write_bytes!(&reg.to_le_bytes());
         }
 
-        // st0 to st7
-        for st_reg in &self.st {
-            write_bytes!(st_reg);
+        write_regs!(pc, msr, cr, lr, ctr, xer, fpscr);
+
+        for &reg in &self.vr {
+            let reg: u128 = reg.into();
+            write_bytes!(&reg.to_le_bytes());
         }
 
-        self.fpu.write(&mut write_byte);
-
-        // xmm0 to xmm15
-        for xmm_reg in &self.xmm {
-            write_bytes!(&xmm_reg.to_le_bytes());
-        }
-
-        // mxcsr
-        write_bytes!(&self.mxcsr.to_le_bytes());
-
-        // padding?
-        // XXX: Couldn't figure out what these do and GDB doesn't actually display any
-        // registers that use these values.
-        (0..0x18).for_each(|_| write_byte(None))
+        write_regs!(vscr, vrsave);
     }
 
     fn gdb_deserialize(&mut self, bytes: &[u8]) -> Result<(), ()> {
-        if bytes.len() < 0x218 {
+        if bytes.len() < 0x3a4 {
             return Err(());
         }
 
-        let mut regs = bytes[0..0x80]
-            .chunks_exact(8)
-            .map(|x| u64::from_le_bytes(x.try_into().unwrap()));
-
-        for reg in self.regs.iter_mut() {
-            *reg = regs.next().ok_or(())?;
-        }
-
-        self.rip = u64::from_le_bytes(bytes[0x80..0x88].try_into().unwrap());
-        self.eflags = u32::from_le_bytes(bytes[0x88..0x8C].try_into().unwrap());
-
-        let mut segments = bytes[0x8C..0xA4]
+        let regs = bytes[0..0x80]
             .chunks_exact(4)
             .map(|x| u32::from_le_bytes(x.try_into().unwrap()));
 
-        for seg in self.segments.iter_mut() {
-            *seg = segments.next().ok_or(())?;
+        for (i, reg) in regs.enumerate() {
+            self.r[i] = reg;
         }
 
-        let mut regs = bytes[0xA4..0xF4].chunks_exact(10).map(TryInto::try_into);
+        let regs = bytes[0x80..0x180]
+            .chunks_exact(8)
+            .map(|x| f64::from_le_bytes(x.try_into().unwrap()));
 
-        for reg in self.st.iter_mut() {
-            *reg = regs.next().ok_or(())?.map_err(|_| ())?;
+        for (i, reg) in regs.enumerate() {
+            self.f[i] = reg;
         }
 
-        self.fpu = bytes[0xF4..0x114].try_into()?;
+        macro_rules! parse_regs {
+            ($start:literal..$end:literal, $($reg:ident),*) => {
+                let mut regs = bytes[$start..$end]
+                    .chunks_exact(4)
+                    .map(|x| u32::from_le_bytes(x.try_into().unwrap()));
+                $(
+                    self.$reg = regs.next().ok_or(())?;
+                )*
+            }
+        }
 
-        let mut regs = bytes[0x114..0x214]
+        parse_regs!(0x180..0x19c, pc, msr, cr, lr, ctr, xer, fpscr);
+
+        let regs = bytes[0x19c..0x39c]
             .chunks_exact(0x10)
             .map(|x| u128::from_le_bytes(x.try_into().unwrap()));
 
-        for reg in self.xmm.iter_mut() {
-            *reg = regs.next().ok_or(())?;
+        for (i, reg) in regs.enumerate() {
+            self.vr[i] = reg.into();
         }
 
-        self.mxcsr = u32::from_le_bytes(bytes[0x214..0x218].try_into().unwrap());
+        parse_regs!(0x39c..0x3a4, vscr, vrsave);
 
         Ok(())
     }
 }
-*/
+
+#[cfg(test)]
+mod ppc_core_tests {
+    use super::*;
+
+    #[test]
+    fn ppc_core_round_trip() {
+        let regs_before = PowerPcCoreRegs {
+            r: [1; 32],
+            pc: 2,
+            msr: 3,
+            cr: 4,
+            lr: 5,
+            ctr: 6,
+            xer: 7,
+            fpscr: 8,
+            f: [9.0; 32],
+            vr: [PpcVector::from([0u16, 1, 2, 3, 4, 5, 6, 7]); 32],
+            vrsave: 10,
+            vscr: 11,
+        };
+
+        let mut data = vec![];
+
+        regs_before.gdb_serialize(|x| {
+            data.push(x.unwrap_or(b'x'));
+        });
+
+        assert_eq!(data.len(), 0x3a4);
+
+        let mut regs_after = PowerPcCoreRegs::default();
+        regs_after.gdb_deserialize(&data).unwrap();
+
+        assert_eq!(regs_before, regs_after);
+    }
+}
