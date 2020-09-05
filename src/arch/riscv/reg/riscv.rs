a2862ab86e55ef9ffada8b4a012c2b9deff8c900
Thomas Hiscock <thomashk000@gmail.com>
2020-09-05 15:19:26 +0200

    complete implementation of the RegId trait for RISC-V
    The RiscvRegId is now public in the arch::riscv::reg module.

--- src/arch/riscv/reg/riscv.rs
+++ src/arch/riscv/reg/riscv.rs
@@ -3,19 +3,27 @@ use crate::internal::LeBytes;
 use num_traits::PrimInt;
 
 /// RISC-V Register identifier.
+#[derive(Debug, Clone)]
 pub enum RiscvRegId {
+    /// General Purpose Register (x0-x31).
     Gpr(u8),
+    /// Floating Point Register (f0-f31).
     Fpr(u8),
+    /// Program Counter.
     Pc,
+    /// Control and Status Register.
     Csr(u16),
+    /// Privilege level.
     Priv,
 }
 
 impl RegId for RiscvRegId {
     fn from_raw_id(id: usize) -> Option<(Self, usize)> {
-        // TODO: fill with missing registers (CSR and FPR)
         match id {
             0..=31 => Some((Self::Gpr(id as u8), 4)),
+            32 => Some((Self::Pc, 4)),
+            33..=64 => Some((Self::Fpr((id - 33) as u8), 4)),
+            65..=4160 => Some((Self::Csr((id - 65) as u16), 4)),
             4161 => Some((Self::Priv, 1)),
             _ => None,
         }
