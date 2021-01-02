58f0605d99cc393e76b6d658be8c1079c5cd7132
starfleetcadet75 <starfleetcadet75@gmail.com>
2021-01-02 18:28:33 -0500

    Implement `RegId` for MSP430

--- src/arch/msp430/mod.rs
+++ src/arch/msp430/mod.rs
@@ -1,23 +1,16 @@
 //! Implementations for the TI-MSP430 family of MCUs.
 
 use crate::arch::Arch;
-use crate::arch::RegId;
 
 pub mod reg;
 
 /// Implements `Arch` for standard 16-bit TI-MSP430 MCUs.
-///
-/// Check out the [module level docs](../index.html#whats-with-regidimpl) for
-/// more info about the `RegIdImpl` type parameter.
-pub enum Msp430<RegIdImpl: RegId> {
-    #[doc(hidden)]
-    _Marker(core::marker::PhantomData<RegIdImpl>),
-}
+pub enum Msp430 {}
 
-impl<RegIdImpl: RegId> Arch for Msp430<RegIdImpl> {
+impl Arch for Msp430 {
     type Usize = u32;
     type Registers = reg::Msp430Regs;
-    type RegId = RegIdImpl;
+    type RegId = reg::id::Msp430RegId;
 
     fn target_description_xml() -> Option<&'static str> {
         Some(r#"<target version="1.0"><architecture>msp430</architecture></target>"#)
