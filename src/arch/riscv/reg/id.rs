6137b4cb4eb715931725fdf6dbfa8c802eb3c72b
Luming Wang <wlm199558@126.com>
2021-03-27 20:14:32 +0800

    fix RiscvRegId's from_raw_id for RISCV64 by using the suggested implementation

--- src/arch/riscv/reg/id.rs
+++ src/arch/riscv/reg/id.rs
@@ -3,7 +3,7 @@ use crate::arch::RegId;
 /// RISC-V Register identifier.
 #[derive(Debug, Clone, Copy)]
 #[non_exhaustive]
-pub enum RiscvRegId {
+pub enum RiscvRegId<U> {
     /// General Purpose Register (x0-x31).
     Gpr(u8),
     /// Floating Point Register (f0-f31).
@@ -14,18 +14,30 @@ pub enum RiscvRegId {
     Csr(u16),
     /// Privilege level.
     Priv,
+
+    #[doc(hidden)]
+    _Marker(core::marker::PhantomData<U>),
 }
 
-impl RegId for RiscvRegId {
-    fn from_raw_id(id: usize) -> Option<(Self, usize)> {
-        let reg_size = match id {
-            0..=31 => (Self::Gpr(id as u8), 4),
-            32 => (Self::Pc, 4),
-            33..=64 => (Self::Fpr((id - 33) as u8), 4),
-            65..=4160 => (Self::Csr((id - 65) as u16), 4),
-            4161 => (Self::Priv, 1),
-            _ => return None,
-        };
-        Some(reg_size)
-    }
+macro_rules! impl_riscv_reg_id {
+    ($usize:ty) => {
+        impl RegId for RiscvRegId<$usize> {
+            fn from_raw_id(id: usize) -> Option<(Self, usize)> {
+                const USIZE: usize = core::mem::size_of::<$usize>();
+
+                let reg_size = match id {
+                    0..=31 => (Self::Gpr(id as u8), USIZE),
+                    32 => (Self::Pc, USIZE),
+                    33..=64 => (Self::Fpr((id - 33) as u8), USIZE),
+                    65..=4160 => (Self::Csr((id - 65) as u16), USIZE),
+                    4161 => (Self::Priv, 1),
+                    _ => return None,
+                };
+                Some(reg_size)
+            }
+        }
+    };
 }
+
+impl_riscv_reg_id!(u32);
+impl_riscv_reg_id!(u64);
