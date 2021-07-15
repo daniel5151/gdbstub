a92b5cc85af719a42332c9cccae4c5e5df5bb3a1
Michael Chesser <michael.chesser@adelaide.edu.au>
2021-07-15 17:39:27 +0930

    Improve MSP430 support.
    - Use the correct number of registers.
    - Add support for CPUX variant (20-bit registers)

--- gdbstub_arch/src/msp430/mod.rs
+++ gdbstub_arch/src/msp430/mod.rs
@@ -9,18 +9,38 @@ pub mod reg;
 ///
 /// Check out the [module level docs](gdbstub::arch#whats-with-regidimpl) for
 /// more info about the `RegIdImpl` type parameter.
-pub enum Msp430<RegIdImpl: RegId = reg::id::Msp430RegId> {
+pub enum Msp430<RegIdImpl: RegId = reg::id::Msp430RegId<u16>> {
     #[doc(hidden)]
     _Marker(core::marker::PhantomData<RegIdImpl>),
 }
 
 impl<RegIdImpl: RegId> Arch for Msp430<RegIdImpl> {
     type Usize = u16;
-    type Registers = reg::Msp430Regs;
+    type Registers = reg::Msp430Regs<u16>;
     type RegId = RegIdImpl;
     type BreakpointKind = usize;
 
     fn target_description_xml() -> Option<&'static str> {
         Some(r#"<target version="1.0"><architecture>msp430</architecture></target>"#)
+    }
+}
+
+/// Implements `Arch` for CPUX 20-bit TI-MSP430 MCUs.
+///
+/// Check out the [module level docs](gdbstub::arch#whats-with-regidimpl) for
+/// more info about the `RegIdImpl` type parameter.
+pub enum Msp430X<RegIdImpl: RegId = reg::id::Msp430RegId<u32>> {
+    #[doc(hidden)]
+    _Marker(core::marker::PhantomData<RegIdImpl>),
+}
+
+impl<RegIdImpl: RegId> Arch for Msp430X<RegIdImpl> {
+    type Usize = u32;
+    type Registers = reg::Msp430Regs<u32>;
+    type RegId = RegIdImpl;
+    type BreakpointKind = usize;
+
+    fn target_description_xml() -> Option<&'static str> {
+        Some(r#"<target version="1.0"><architecture>msp430x</architecture></target>"#)
     }
 }
--- gdbstub_arch/src/msp430/reg/id.rs
+++ gdbstub_arch/src/msp430/reg/id.rs
@@ -6,7 +6,7 @@ use gdbstub::arch::RegId;
 /// The best file to reference is [msp430-tdep.c](https://github.com/bminor/binutils-gdb/blob/master/gdb/msp430-tdep.c).
 #[derive(Debug, Clone, Copy)]
 #[non_exhaustive]
-pub enum Msp430RegId {
+pub enum Msp430RegId<U> {
     /// Program Counter (R0)
     Pc,
     /// Stack Pointer (R1)
@@ -17,19 +17,33 @@ pub enum Msp430RegId {
     Cg,
     /// General Purpose Registers (R4-R15)
     Gpr(u8),
+    #[doc(hidden)]
+    _Size(U),
 }
 
-impl RegId for Msp430RegId {
+fn from_raw_id<U>(id: usize) -> Option<(Msp430RegId<U>, usize)> {
+    let reg = match id {
+        0 => Msp430RegId::Pc,
+        1 => Msp430RegId::Sp,
+        2 => Msp430RegId::Sr,
+        3 => Msp430RegId::Cg,
+        4..=15 => Msp430RegId::Gpr((id as u8) - 4),
+        _ => return None,
+    };
+
+    let ptrsize = core::mem::size_of::<U>();
+    Some((reg, ptrsize))
+}
+
+impl RegId for Msp430RegId<u16> {
     fn from_raw_id(id: usize) -> Option<(Self, usize)> {
-        let reg = match id {
-            0 => Self::Pc,
-            1 => Self::Sp,
-            2 => Self::Sr,
-            3 => Self::Cg,
-            4..=15 => Self::Gpr((id as u8) - 4),
-            _ => return None,
-        };
-        Some((reg, 2))
+        from_raw_id::<u16>(id)
+    }
+}
+
+impl RegId for Msp430RegId<u32> {
+    fn from_raw_id(id: usize) -> Option<(Self, usize)> {
+        from_raw_id::<u32>(id)
     }
 }
 
