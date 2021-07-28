c40145cda67016ecf383fa5f64a2b6c3fc4aecf2
Bet4 <0xbet4@gmail.com>
2021-07-29 00:05:37 +0800

    Unify the way of writing a range of data (#64)

--- src/gdbstub_impl/ext/memory_map.rs
+++ src/gdbstub_impl/ext/memory_map.rs
@@ -17,20 +17,8 @@ impl<T: Target, C: Connection> GdbStubImpl<T, C> {
 
         let handler_status = match command {
             MemoryMap::qXferMemoryMapRead(cmd) => {
-                let xml = ops.memory_map_xml().trim();
-                if cmd.offset >= xml.len() {
-                    // no more data
-                    res.write_str("l")?;
-                } else if cmd.offset + cmd.len >= xml.len() {
-                    // last little bit of data
-                    res.write_str("l")?;
-                    res.write_binary(&xml.as_bytes()[cmd.offset..])?
-                } else {
-                    // still more data
-                    res.write_str("m")?;
-                    res.write_binary(&xml.as_bytes()[cmd.offset..(cmd.offset + cmd.len)])?
-                }
-
+                let xml = ops.memory_map_xml().trim().as_bytes();
+                res.write_binary_range(xml, cmd.offset, cmd.len)?;
                 HandlerStatus::Handled
             }
         };
