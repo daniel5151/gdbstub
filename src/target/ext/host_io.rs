4a636c4b8e824d6a676a7875ad1a4b13db11d851
Mariusz Kryński <mrk@sed.pl>
2022-02-22 10:02:29 +0100

    update docs

--- src/target/ext/host_io.rs
+++ src/target/ext/host_io.rs
@@ -8,7 +8,9 @@ bitflags! {
     /// Host flags for opening files.
     ///
     /// Extracted from the GDB documentation at
-    /// [Open Flags](https://sourceware.org/gdb/current/onlinedocs/gdb/Open-Flags.html#Open-Flags)
+    /// [Open Flags](https://sourceware.org/gdb/current/onlinedocs/gdb/Open-Flags.html#Open-Flags),
+    /// and the LLDB source code at
+    /// [`lldb/include/lldb/Host/File.h`](https://github.com/llvm/llvm-project/blob/ec642ceebc1aacc8b16249df7734b8cf90ae2963/lldb/include/lldb/Host/File.h#L47-L66)
     pub struct HostIoOpenFlags: u32 {
         /// A read-only file.
         const O_RDONLY = 0x0;
@@ -25,16 +27,13 @@ bitflags! {
         /// Exclusive access.
         const O_EXCL = 0x800;
 
-        // LLDB-specific flags, as documented at:
-        // https://github.com/llvm/llvm-project/blob/ec642ceebc1aacc8b16249df7734b8cf90ae2963/lldb/include/lldb/Host/File.h#L47-L66
-
-        /// Do not block
+        /// LLDB extension: Do not block.
         const O_NONBLOCK = 1 << 28;
-        /// Do not follow symlinks
+        /// LLDB extension: Do not follow symlinks.
         const O_DONT_FOLLOW_SYMLINKS = 1 << 29;
-        /// Close the file when executing a new process
+        /// LLDB extension: Close the file when executing a new process.
         const O_CLOSE_ON_EXEC = 1 << 30;
-        /// Invalid value
+        /// LLDB extension: Invalid value.
         const O_INVALID = 1 << 31;
     }
 }
