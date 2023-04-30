2cf3299476f236b366a4e7e0bf02f8a184e80039
Daniel Prilik <danielprilik@gmail.com>
2023-04-30 11:50:35 -0700

    dump_asm.sh improvements
    log if no panics were found
    
    trim debug sections from asm
    
    add EXTRA_TRIM to dump_asm

--- example_no_std/dump_asm.sh
+++ example_no_std/dump_asm.sh
@@ -8,13 +8,22 @@ then
     cargo install rustfilt
 fi
 
+rm -rf ./target
 cargo rustc --release -- --emit asm -C "llvm-args=-x86-asm-syntax=intel"
 cat ./target/release/deps/gdbstub_nostd-*.s | rustfilt > asm.s
 sed -i -E '/\.(cfi_def_cfa_offset|cfi_offset|cfi_startproc|cfi_endproc|size)/d' asm.s
+
+if [ -n "$EXTRA_TRIM" ]; then
+    sed -i -E '/\.(Ltmp|file|loc)/d' asm.s
+    sed -i -E '/.section\t.debug_loc/,$d' asm.s
+fi
+
 echo "asm emitted to asm.s"
 
 if grep "core::panicking::panic_fmt" asm.s
 then
     echo "found panic in example_no_std!"
     exit 1
+else
+    echo "no panics in example_no_std"
 fi
