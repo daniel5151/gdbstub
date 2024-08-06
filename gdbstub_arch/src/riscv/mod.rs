641e687ab5c6da41e4fbbebe62ca26670d707279
Daniel Lehmann <59584561+danlehmann@users.noreply.github.com>
2024-08-06 11:28:01 -0700

    RISC-V: List all integer registers for lldb compatibility (#149)
    * lldb/risc-v: Add all integer registers
    
    lldb requires a full list of registers in the target_description_xml (gdb
    on the other hand seems to ignore the list completely).
    
    The gdb manual sounds like these registers should exist anyway:
    > The ‘org.gnu.gdb.riscv.cpu’ feature is required for RISC-V targets. It should contain the registers ‘x0’ through ‘x31’, and ‘pc’.
    
    Therefore it seems reasonable that the RISC-V arch returns the full list.
    
    * Include original files via include_str! instead

--- gdbstub_arch/src/riscv/mod.rs
+++ gdbstub_arch/src/riscv/mod.rs
@@ -15,21 +15,21 @@ pub enum Riscv64 {}
 impl Arch for Riscv32 {
     type Usize = u32;
     type Registers = reg::RiscvCoreRegs<u32>;
-    type RegId = reg::id::RiscvRegId<u32>;
     type BreakpointKind = usize;
+    type RegId = reg::id::RiscvRegId<u32>;
 
     fn target_description_xml() -> Option<&'static str> {
-        Some(r#"<target version="1.0"><architecture>riscv:rv32</architecture></target>"#)
+        Some(include_str!("rv32i.xml"))
     }
 }
 
 impl Arch for Riscv64 {
     type Usize = u64;
     type Registers = reg::RiscvCoreRegs<u64>;
-    type RegId = reg::id::RiscvRegId<u64>;
     type BreakpointKind = usize;
+    type RegId = reg::id::RiscvRegId<u64>;
 
     fn target_description_xml() -> Option<&'static str> {
-        Some(r#"<target version="1.0"><architecture>riscv:rv64</architecture></target>"#)
+        Some(include_str!("rv64i.xml"))
     }
 }
