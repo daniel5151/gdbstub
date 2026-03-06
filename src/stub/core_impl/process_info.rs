dbd088763c83dc839cea16bc73e393fd68962fec
Chris Fallin <chris@cfallin.org>
2026-03-05 20:54:40 -0800

    Review feedback.

--- src/stub/core_impl/process_info.rs
+++ src/stub/core_impl/process_info.rs
@@ -1,5 +1,39 @@
 use super::prelude::*;
+use crate::common::Endianness;
 use crate::protocol::commands::ext::ProcessInfo;
+use crate::protocol::ResponseWriterError;
+use crate::target::ext::process_info::InfoResponse;
+
+impl<'a> InfoResponse<'a> {
+    fn write_response<C: Connection>(
+        &self,
+        res: &mut ResponseWriter<'_, C>,
+    ) -> Result<(), ResponseWriterError<C::Error>> {
+        match self {
+            InfoResponse::Pid(pid) => {
+                res.write_str("pid:")?;
+                res.write_dec(usize::from(*pid))?;
+            }
+            InfoResponse::Triple(triple) => {
+                res.write_str("triple:")?;
+                res.write_hex_buf(triple.as_bytes())?;
+            }
+            InfoResponse::Endianness(endian) => {
+                res.write_str("endian:")?;
+                res.write_str(match endian {
+                    Endianness::Big => "big;",
+                    Endianness::Little => "little;",
+                })?;
+            }
+            InfoResponse::PointerSize(p) => {
+                res.write_str("ptrsize:")?;
+                res.write_dec(*p)?;
+            }
+        }
+        res.write_str(";")?;
+        Ok(())
+    }
+}
 
 impl<T: Target, C: Connection> GdbStubImpl<T, C> {
     pub(crate) fn handle_process_info(
@@ -15,25 +49,25 @@ impl<T: Target, C: Connection> GdbStubImpl<T, C> {
 
         crate::__dead_code_marker!("process_info", "impl");
 
-        let mut write_err = Ok(());
-        let mut write_cb = |data: &[u8]| {
-            if write_err.is_ok() {
-                if let Err(e) = res.write_str(core::str::from_utf8(data).unwrap_or("")) {
-                    write_err = Err(e);
+        let mut result = Ok(());
+        let mut write_info = |info: &InfoResponse<'_>| {
+            if result.is_ok() {
+                if let Err(e) = info.write_response(res) {
+                    result = Err(e);
                 }
             }
         };
 
         let handler_status = match command {
             ProcessInfo::qHostInfo(_cmd) => {
-                ops.host_info(&mut write_cb).map_err(Error::TargetError)?;
-                write_err?;
+                ops.host_info(&mut write_info).map_err(Error::TargetError)?;
+                result?;
                 HandlerStatus::Handled
             }
             ProcessInfo::qProcessInfo(_cmd) => {
-                ops.process_info(&mut write_cb)
+                ops.process_info(&mut write_info)
                     .map_err(Error::TargetError)?;
-                write_err?;
+                result?;
                 HandlerStatus::Handled
             }
         };
--- src/target/ext/process_info.rs
+++ src/target/ext/process_info.rs
@@ -2,33 +2,45 @@
 //!
 //! These correspond to the `qHostInfo` and `qProcessInfo` commands.
 //! They report key-value metadata such as the target triple,
-//! endianness, pointer size, and process ID.
-//!
-//! The callback passed to these methods should be called with byte slices
-//! that together form a semicolon-delimited `key:value;` response string.
-//! For example:
-//!
-//! ```text
-//! triple:7761736d33322d756e6b6e6f776e2d756e6b6e6f776e2d7761736d;pid:1;endian:little;ptrsize:4;
-//! ```
-//!
-//! Note: the `triple` value must be hex-encoded.
+//! endianness, pointer size, and process ID. We take the information
+//! that we can return as responses to this commands in the
+//! [`HostInfoResponse`] and [`ProcessInfoResponse`] structs,
+//! respectively.
+
+use crate::common::{Endianness, Pid};
+
+/// A response key-value pair to a qProcessInfo or qHostInfo query.
+///
+/// A response to either of these commands consists of a list of
+/// key-value pairs, each of which is represented by one instance of
+/// this enum.
+pub enum InfoResponse<'a> {
+    /// The current process PID.
+    Pid(Pid),
+    /// The target triple for the debuggee, as a string.
+    Triple(&'a str),
+    /// The target endianness.
+    Endianness(Endianness),
+    /// The pointer size.
+    PointerSize(usize),
+}
 
 use crate::target::Target;
 
 /// Target Extension - Provide host and process information.
 pub trait ProcessInfo: Target {
-    /// Write the response to `qHostInfo`.
+    /// Write a response to `qHostInfo`.
     ///
-    /// Call `write` one or more times with byte slices that together form
-    /// the response. Each call appends to the output.
-    fn host_info(&self, write: &mut dyn FnMut(&[u8])) -> Result<(), Self::Error>;
+    /// Call `write_item` with each `InfoResponse` you wish to send.
+    fn host_info(&self, write_item: &mut dyn FnMut(&InfoResponse<'_>)) -> Result<(), Self::Error>;
 
     /// Write the response to `qProcessInfo`.
     ///
-    /// Call `write` one or more times with byte slices that together form
-    /// the response. Each call appends to the output.
-    fn process_info(&self, write: &mut dyn FnMut(&[u8])) -> Result<(), Self::Error>;
+    /// Call `write_item` with each `InfoResponse` you wish to send.
+    fn process_info(
+        &self,
+        write_item: &mut dyn FnMut(&InfoResponse<'_>),
+    ) -> Result<(), Self::Error>;
 }
 
 define_ext!(ProcessInfoOps, ProcessInfo);
