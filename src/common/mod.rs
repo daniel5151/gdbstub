c312107a4d5ba4c4dfca1062ed3e0340a26980a4
Daniel Prilik <danielprilik@gmail.com>
2026-03-11 10:43:08 -0400

    clarify rationale behind `Tid`/`Pid` repr
    closes #193

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -3,14 +3,36 @@
 mod signal;
 
 pub use self::signal::Signal;
+use core::num::NonZeroUsize;
 
-/// Thread ID
-pub type Tid = core::num::NonZeroUsize;
+/// Thread ID (as viewed by GDB)
+///
+/// The choice to use a [`NonZeroUsize`] stems from the [GDB RSP Packet
+/// documentation], which states that thread IDs are "positive numbers with a
+/// target-specific interpretation".
+///
+/// Target implementations may wish to map `Tid`s to/from their own
+/// target-specific thread ID type. (e.g: an emulator might treat `Tid` as a CPU
+/// index).
+///
+/// [GDB RSP Packet documentation]:
+///     https://sourceware.org/gdb/current/onlinedocs/gdb.html/Packets.html#Packets
+pub type Tid = NonZeroUsize;
 
-/// Process ID
-pub type Pid = core::num::NonZeroUsize;
+/// Process ID (as viewed by GDB)
+///
+/// The choice to use a [`NonZeroUsize`] stems from the [GDB RSP Packet
+/// documentation], which states that process IDs are "positive numbers with a
+/// target-specific interpretation".
+///
+/// Target implementations may wish to map `Pid`s to/from their own
+/// target-specific process ID type.
+///
+/// [GDB RSP Packet documentation]:
+///     https://sourceware.org/gdb/current/onlinedocs/gdb.html/Packets.html#Packets
+pub type Pid = NonZeroUsize;
 
-/// Endinanness.
+/// Endianness.
 ///
 /// This is used to report target endianness to the debugger as a
 /// response to certain commands.
